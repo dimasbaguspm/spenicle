89f25b1557f9918553b2fd539f1804ebc6e16175
dimasbaguspm <dimas.bagus.pm1@gmail.com>
2025-06-15 10:36:42 +0700

    refactor: remove unused process.env.STAGE definition from Vite configuration

--- packages/web/vite.config.js
+++ packages/web/vite.config.js
@@ -8,11 +8,6 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [TanStackRouterVite({ autoCodeSplitting: true }), viteReact(), tailwindcss()],
-  define: {
-    'process.env': {
-      STAGE: process.env.WEB_STAGE,
-    },
-  },
   server: {
     port: process.env.WEB_PORT,
   },
