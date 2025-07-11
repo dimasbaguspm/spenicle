22fb9784039f9d888aef8704aaaf6db836d926a7
dimasbaguspm <dimas.bagus.pm1@gmail.com>
2025-07-12 06:03:34 +0700

    feat: pwa and manifest

--- packages/web/vite.config.js
+++ packages/web/vite.config.js
@@ -4,10 +4,17 @@ import tailwindcss from '@tailwindcss/vite';
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 import viteReact from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [TanStackRouterVite({ autoCodeSplitting: true }), viteReact(), tailwindcss()],
+  plugins: [
+    TanStackRouterVite({ autoCodeSplitting: true }),
+    viteReact(),
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+    }),
+  ],
   server: {
     port: process.env.WEB_PORT,
   },
