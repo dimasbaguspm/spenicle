15d87fde329034092647f2ab27aa47c0a2bf38b2
dimasbaguspm <dimas.bagus.pm1@gmail.com>
2026-01-27 16:00:04 +0700

    refactor(database): consolidate migration logic and remove redundant migrations file

--- apps/backend/internal/configs/database.go
+++ apps/backend/internal/configs/database.go
@@ -2,33 +2,64 @@ package configs
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"log/slog"
+	"os"
+	"path/filepath"
 
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 type Database struct {
-	env Environment
 }
 
-func NewDatabase(env Environment) Database {
-	return Database{
-		env,
-	}
-}
+func NewDatabase(ctx context.Context, env Environment) *pgxpool.Pool {
+	pool, err := pgxpool.New(ctx, env.DatabaseURL)
 
-func (db Database) Connect(ctx context.Context) (*pgxpool.Pool, error) {
-	pool, err := pgxpool.New(ctx, db.env.DatabaseURL)
 	if err != nil {
 		slog.Info("Unable to create pool connection")
-		return nil, err
+		os.Exit(1)
+		return nil
 	}
 
+	slog.Info("Checking DB conection")
 	if err := pool.Ping(ctx); err != nil {
 		slog.Info("Db is unreachable")
 		pool.Close()
-		return nil, err
+		os.Exit(1)
+		return nil
+	}
+	slog.Info("Database is in healthy condition")
+
+	slog.Info("Running migration")
+	runMigration(env)
+	slog.Info("Migration success")
+
+	return pool
+}
+
+func runMigration(env Environment) error {
+	migrator, err := migrate.New(fmt.Sprintf("file://%s", filepath.Clean("migrations")), env.DatabaseURL)
+
+	if err != nil {
+		slog.Error("Something wrong with migrator", "error", err)
+		os.Exit(1)
+		return err
 	}
 
-	return pool, nil
+	defer migrator.Close()
+
+	if err := migrator.Up(); err != nil {
+		if errors.Is(err, migrate.ErrNoChange) {
+			slog.Info("Migration is done, no changed schemas are detected")
+			return nil
+		}
+		slog.Error("Unable to run migration", "error", err)
+		return err
+	}
+	return nil
 }
